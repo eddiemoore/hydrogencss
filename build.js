beb68ee38174f572f70c36200d828b3ce517e07c
Ed Moore <eddie.moore@gmail.com>
2017-04-07 11:25:32 +1000

    Fixing data.

--- build.js
+++ build.js
@@ -1,15 +1,16 @@
 const fs = require('fs')
 const path = require('path')
-const request = require('request')
 const parse = require('css-tree/lib/lexer/grammar/parse')
-
-const PATH = 'https://raw.githubusercontent.com/mdn/data/master/css/properties.json'
+const data = require('css-tree/data')
 
 const generateContent = prop => {
-  const content = prop.terms.map(term => ({
-    name: term.name,
-    content: `.${term.name}{${prop.name}: ${term.name};}`
-  }))
+  const content = prop.terms.map(value => {
+    const className = !Number.isNaN(parseInt(value.name, 10)) ? `n${value.name}` : value.name
+    return {
+      name: value.name,
+      content: `.${className}{${prop.name}:${value.name};}`
+    }
+  })
   return {
     name: prop.name,
     content,
@@ -40,22 +41,17 @@ const createJoinedFile = prop => {
 }
 
 if (process.argv[1].split('/').pop().includes('build')) {
-  request(PATH, (req, res) => {
-    if (res.statusCode >= 400) throw Error('Failed to load properties')
-
-    const json = JSON.parse(res.body)
-    Object
-      .keys(json)
-      .map(key => Object.assign({}, { name: key }, parse(json[key].syntax)))
-      .map(prop => Object.assign({}, prop, {
-        terms: prop.terms.filter(term => term.type === 'Keyword')
-      }))
-      .filter(prop => prop.terms.length > 0)
-      .map(generateContent)
-      .forEach(prop => {
-        createDirectory(prop)
-        createFiles(prop)
-        createJoinedFile(prop)
-      })
-  })
+  Object
+    .keys(data.properties)
+    .map(key => Object.assign({}, { name: key }, parse(data.properties[key])))
+    .map(prop => Object.assign({}, prop, {
+      terms: prop.terms.filter(term => term.type === 'Keyword')
+    }))
+    .filter(prop => prop.terms.length > 0)
+    .map(generateContent)
+    .forEach(prop => {
+      createDirectory(prop)
+      createFiles(prop)
+      createJoinedFile(prop)
+    })
 }
